1eeb00ed3aac98076b502fbfe5cad075a978b061
David Phillips <david@davidphillips.email>
2014-12-14 00:55:37 -0800

    Now uses goslack.SendMessage to send message to channel

--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 
 	"github.com/daph/goslack"
 )
-
-type SlackMessage struct {
-	Id      int    `json:"id"`
-	Type    string `json:"type"`
-	Channel string `json:"channel"`
-	Text    string `json:"text"`
-}
 
 type Config struct {
 	token   string
@@ -44,20 +36,10 @@ func main() {
 		os.Exit(-1)
 	}
 	defer ws.Close()
-	var message SlackMessage
+	var message goslack.SlackMessage
 	message.Id += 1
 	message.Type = "message"
 	message.Channel = conf.channel
 	message.Text = "hello slack"
-	b_message, err := json.Marshal(message)
-	if err != nil {
-		fmt.Println("Could not marshal the message. ERR: %v", err)
-		os.Exit(-1)
-	}
-	_, err = ws.Write(b_message)
-	if err != nil {
-		fmt.Println("Couldn't write message. ERR: %v", err)
-		os.Exit(-1)
-	}
-
+	goslack.SendMessage(ws, message)
 }
