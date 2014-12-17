8c21d4f61894bcc03431d85c21e6008b43b13be2
David Phillips <david@davidphillips.email>
2014-12-17 11:56:18 -0800

    Change commands from @gopher to !

--- handleMessage.go
+++ handleMessage.go
@@ -11,23 +11,24 @@ import (
 func handleMessage(msg goslack.Event, client *goslack.Client) {
 	// If the message isn't of type message or is sent my the bot user or whas not sent by @<user>
 	// then return
-	if msg.Type != "message" || !strings.Contains(msg.Text, client.Self.Id) {
+	if msg.Type != "message" || msg.User == client.Self.Id || len(strings.Split(msg.Text, "")) <= 0 || strings.Split(msg.Text, "")[0] != "!" {
 		return
 	}
 
-	command := strings.Split(msg.Text, " ")
+	temp := strings.Join(strings.Split(msg.Text, "")[1:], "")
+	command := strings.Split(temp, " ")
 	if len(command) < 2 {
 		client.PushMessage(msg.Channel, "herp")
 		return
 	}
 
-	switch command[1] {
+	switch command[0] {
 	case "figlet":
-		if len(command) < 3 {
+		if len(command) < 2 {
 			client.PushMessage(msg.Channel, "herp")
 			return
 		}
-		output, err := figlet(command[2:])
+		output, err := figlet(command[1:])
 		if err != nil {
 			client.PushMessage(msg.Channel, fmt.Sprintf("There was an error running your command. ERR: %v", err))
 			return
@@ -35,30 +36,30 @@ func handleMessage(msg goslack.Event, client *goslack.Client) {
 		client.PushMessage(msg.Channel, "```"+output+"```")
 
 	case "ud":
-		if len(command) < 3 {
+		if len(command) < 2 {
 			client.PushMessage(msg.Channel, "herp")
 			return
 		}
-		client.PushMessage(msg.Channel, ud(command[2:]))
+		client.PushMessage(msg.Channel, ud(command[1:]))
 
 	case "google":
-		if len(command) < 3 {
+		if len(command) < 2 {
 			client.PushMessage(msg.Channel, "herp")
 			return
 		}
-		client.PushMessage(msg.Channel, google(command[2:]))
+		client.PushMessage(msg.Channel, google(command[1:]))
 
 	case "giphy":
-		if len(command) < 3 {
+		if len(command) < 2 {
 			client.PushMessage(msg.Channel, "herp")
 			return
 		}
-		client.PushMessage(msg.Channel, giphy(command[2:]))
+		client.PushMessage(msg.Channel, giphy(command[1:]))
 	case "randomgif":
 		client.PushMessage(msg.Channel, randomgif())
 
 	default:
-		client.PushMessage(msg.Channel, "derp")
+		client.PushMessage(msg.Channel, string(temp)+" "+string(command[1]))
 	}
 
 }
@@ -73,7 +74,8 @@ func figlet(command []string) (string, error) {
 }
 
 func ud(command []string) string {
-	return "http://www.urbandictionary.com/define.php?term=" + command[0]
+	query := strings.Join(command, "%20")
+	return "http://www.urbandictionary.com/define.php?term=" + query
 }
 
 func google(command []string) string {
